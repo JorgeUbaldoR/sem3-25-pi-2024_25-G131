943c7a813f4551f78e609f0775a1ea672e0202cc
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-10-11 16:21:18 +0100

    ID/OperationQueue Classes - Documentation

--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/OperationQueue.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/OperationQueue.java
@@ -5,32 +5,58 @@ import pt.ipp.isep.dei.esoft.project.domain.more.Operation;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Represents a queue of items for a specific operation.
+ * This class manages the queue of items that need to be processed for a given operation.
+ */
 public class OperationQueue {
-    private Operation operation;
-    private Queue<Item> itemList;
+    private final Operation operation;
+    private final Queue<Item> itemList;
 
+    /**
+     * Constructs an OperationQueue for a specified operation.
+     *
+     * @param operation the operation associated with this queue.
+     */
     public OperationQueue(Operation operation) {
         this.operation = operation;
         itemList = new LinkedList<>();
     }
 
+    /**
+     * Gets the operation associated with this queue.
+     *
+     * @return the operation associated with this queue.
+     */
     public Operation getOperation() {
         return operation;
     }
 
+    /**
+     * Adds an item to the end of the queue.
+     *
+     * @param item the item to be added to the queue.
+     */
     public void addItemToQueue(Item item) {
         itemList.add(item);
     }
 
+    /**
+     * Retrieves and removes the next item from the queue.
+     *
+     * @return the next item in the queue, or null if the queue is empty.
+     */
     public Item getNextItem(){
         return itemList.poll();
     }
 
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return true if the queue is empty; false otherwise.
+     */
     public boolean isEmpty() {
         return itemList.isEmpty();
     }
 
-    public Queue<Item> getItemList() {
-        return itemList;
-    }
 }
--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/more/ID.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/more/ID.java
@@ -9,28 +9,55 @@ public class ID implements Comparable<ID> {
     private TypeID typeID;
     private String keyID;
 
+    /**
+     * Constructs an ID with a serial number and a type.
+     *
+     * @param serial the serial number of the ID.
+     * @param typeID the type of the ID.
+     */
     public ID(int serial, TypeID typeID) {
         this.serial = serial;
         this.typeID = typeID;
         this.keyID = typeID.toString() + "-" + serial;
     }
-    //-----
 
-
+    /**
+     * Returns the type of the ID.
+     *
+     * @return the typeID of this ID.
+     */
     public TypeID getTypeID() {
         return typeID;
     }
 
+    /**
+     * Returns the serial number of the ID.
+     *
+     * @return the serial number of this ID.
+     */
     public int getSerial() {
         return serial;
     }
 
+    /**
+     * Returns the keyID string representation of the ID.
+     *
+     * @return the keyID of this ID.
+     */
     public String getKeyID() {
         return keyID;
     }
 
 
-    //-----
+    //---------- Overrides and clone methods
+
+    /**
+     * Compares this ID to the specified object for equality.
+     * Two IDs are considered equal if they have the same keyID.
+     *
+     * @param o the object to compare this ID against.
+     * @return true if the specified object is equal to this ID; false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -41,27 +68,50 @@ public class ID implements Comparable<ID> {
         return Objects.equals(keyID, id.keyID);
     }
 
+    /**
+     * Returns a hash code value for this ID.
+     * The hash code is based on the keyID.
+     *
+     * @return a hash code value for this ID.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(keyID);
     }
 
+    /**
+     * Returns a string representation of the ID.
+     *
+     * @return the keyID as a string.
+     */
     @Override
     public String toString() {
         return String.format("%s",keyID);
     }
 
-    public ID clone() {
-        return new ID(serial, typeID);
-    }
-
+    /**
+     * Compares this ID with another ID for order.
+     * The IDs are compared based on their typeID and serial number.
+     *
+     * @param otherID the ID to be compared.
+     * @return a negative integer, zero, or a positive integer as this ID
+     *         is less than, equal to, or greater than the specified ID.
+     * @throws IllegalArgumentException if the other ID has a different typeID.
+     */
+    @Override
     public int compareTo(ID otherID) {
         if (!this.typeID.toString().equals(otherID.typeID.toString()))
             throw new IllegalArgumentException("Can't compare two different types of key.");
-        if (serial > otherID.serial)
-            return 1;
-        if (serial < otherID.serial)
-            return -1;
-        return 0;
+        return Integer.compare(serial, otherID.serial);
+    }
+
+    /**
+     * Creates and returns a clone of this ID.
+     * The cloned ID has the same serial and type.
+     *
+     * @return a clone of this ID.
+     */
+    public ID clone() {
+        return new ID(serial, typeID);
     }
 }
