85525eb920bb62990e9a4711cd12425a71a9fa60
Francisco Santos <1230564@isep.ipp.pt>
2024-10-11 16:55:52 +0100

    Documentation for OperationController class

--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/application/controller/OperationController.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/application/controller/OperationController.java
@@ -14,6 +14,11 @@ public class OperationController {
     private OperationRepository operationRepository;
     private final MachineRepository machineRepository;
 
+    /**
+     * Constructor for OperationController.
+     * Initializes machineRepository and operationRepository.
+     * Also fills operations from machines upon creation.
+     */
     public OperationController() {
         this.machineRepository = getMachineRepository();
         this.operationRepository = getOperationRepository();
@@ -21,6 +26,11 @@ public class OperationController {
 
     }
 
+    /**
+     * Retrieves the OperationRepository instance.
+     *
+     * @return the operationRepository
+     */
     private OperationRepository getOperationRepository() {
         if (operationRepository == null) {
             Repositories repository = Repositories.getInstance();
@@ -29,19 +39,38 @@ public class OperationController {
         return operationRepository;
     }
 
+    /**
+     * Retrieves the MachineRepository instance.
+     *
+     * @return the machineRepository
+     */
     private MachineRepository getMachineRepository() {
         Repositories repository = Repositories.getInstance();
         return repository.getMachineRepository();
     }
 
+    /**
+     * Adds a new operation to the operation repository.
+     *
+     * @param operation the operation to be added
+     * @return an Optional containing the added operation if successful
+     */
     public Optional<Operation> addOperation(Operation operation) {
         return getOperationRepository().addOperation(operation);
     }
 
+    /**
+     * Retrieves a list of all machines.
+     *
+     * @return a list of machines
+     */
     public List<Machine> getAllMachines() {
         return machineRepository.getMachineList();
     }
 
+    /**
+     * Fills operations in the operation repository based on the available machines.
+     */
     public void fillOperationsFromMachines() {
         List<Machine> machines = getAllMachines();
         operationRepository.fillOperations(machines);
