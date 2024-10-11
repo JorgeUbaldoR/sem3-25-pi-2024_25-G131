34246e3f87283f0c8f3c856d546eb4988d4b0020
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-10-11 16:09:38 +0100

    Item/Machine Classes - Documentation

--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/Item.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/Item.java
@@ -6,6 +6,10 @@ import pt.ipp.isep.dei.esoft.project.domain.more.Operation;
 
 import java.util.*;
 
+/**
+ * Represents an item that has a unique ID, a priority level,
+ * and a list of operations it needs to undergo.
+ */
 public class Item {
     private ID itemID;
     private Priority priority;
@@ -13,7 +17,13 @@ public class Item {
     private Operation currentOperation;
 
 
-
+    /**
+     * Constructs an Item with the specified ID, priority, and list of operations.
+     *
+     * @param itemID        the unique identifier for the item.
+     * @param priority      the priority level of the item.
+     * @param operationList the queue of operations that the item will undergo.
+     */
     public Item(ID itemID, Priority priority, Queue<Operation> operationList) {
         this.itemID = itemID;
         this.priority = priority;
@@ -22,28 +32,30 @@ public class Item {
     }
 
 
-    //-- Set
-    public void setItemID(ID itemID) {
-        this.itemID = itemID;
-    }
-    public void setPriority(Priority priority) {
-        this.priority = priority;
-    }
 
-    //-- Gets
+    /**
+     * Gets the unique identifier for this item.
+     *
+     * @return the item's ID.
+     */
     public ID getItemID() {
         return itemID;
     }
-    public Priority getPriority() {
-        return priority;
-    }
-    public Queue<Operation> getOperationList() {
-        return operationList;
-    }
+
+    /**
+     * Gets the current operation that this item is undergoing.
+     *
+     * @return the current operation, or null if there is none.
+     */
     public Operation getCurrentOperation() {
         return currentOperation;
     }
 
+    /**
+     * Gets the next operation in the queue for this item.
+     *
+     * @return the next operation, or null if there are no more operations.
+     */
     public Operation getNextOperation() {
         if (!operationList.isEmpty()) {
             operationList.poll();
@@ -53,29 +65,50 @@ public class Item {
         return null;
     }
 
+    /**
+     * Creates a clone of this item.
+     *
+     * @return a new Item object that is a copy of this item.
+     */
+    public Item clone() {
+        return new Item(itemID, priority, new LinkedList<>(operationList));
+    }
 
-
-    //-- Methods of class Machine
+    /**
+     * Compares this item to the specified object for equality.
+     * Two items are considered equal if they have the same ID.
+     *
+     * @param o the object to compare this item against.
+     * @return true if the specified object is equal to this item; false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         Item item = (Item) o;
-        return Objects.equals(itemID, item.itemID) && priority == item.priority && Objects.equals(operationList, item.operationList);
+        return itemID.equals(item.itemID);
     }
 
+    /**
+     * Returns a hash code value for this item.
+     * The hash code is based on the item's unique ID.
+     *
+     * @return a hash code value for this item.
+     */
     @Override
     public int hashCode() {
         return itemID.hashCode();
     }
 
+    /**
+     * Returns a string representation of this item.
+     * The string includes the item's ID, priority, and the next operation in the queue.
+     *
+     * @return a string representation of the item.
+     */
     @Override
     public String toString() {
-        return String.format("%s -> %s -> %s ",itemID, priority, operationList.peek());
-    }
-
-    public Item clone(){
-        return new Item(itemID, priority, new LinkedList<>(operationList));
+        return String.format("%s -> %s -> %s ", itemID, priority, operationList.peek());
     }
 }
--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/Machine.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/Machine.java
@@ -16,7 +16,13 @@ public class Machine implements Comparable<Machine> {
     private float timeLeftToFinish;
     private Item currentProcessingItem;
 
-
+    /**
+     * Constructs a new Machine with the specified ID, operation, and processing speed.
+     *
+     * @param id_machine the unique identifier for the machine.
+     * @param operation the operation associated with the machine.
+     * @param processingSpeed the processing speed of the machine.
+     */
     public Machine(ID id_machine, Operation operation, float processingSpeed) {
         this.id_machine = id_machine;
         this.operation = operation;
@@ -26,6 +32,11 @@ public class Machine implements Comparable<Machine> {
         this.currentProcessingItem = null;
     }
 
+    /**
+     * Processes the specified item if the operation of the item matches the machine's operation.
+     *
+     * @param item the item to be processed by this machine.
+     */
     public void processItem(Item item) {
         Operation processingOperation = item.getCurrentOperation();
         if (processingOperation != null && processingOperation.equals(this.operation)) {
@@ -38,7 +49,12 @@ public class Machine implements Comparable<Machine> {
         }
     }
 
-
+    /**
+     * Updates the machine's status, decrementing the time left to finish processing.
+     * If the processing is complete, it resets the machine and returns true.
+     *
+     * @return true if the machine has finished processing; false otherwise.
+     */
     public boolean updateMachine() {
         if (!available) {
             this.timeLeftToFinish--;
@@ -51,11 +67,18 @@ public class Machine implements Comparable<Machine> {
         return false;
     }
 
+    /**
+     * Resets the machine's status to available and time left to finish to default.
+     */
     private void resetMachine() {
         this.timeLeftToFinish = DEFAULT_TIME_LEFT;
         this.available = DEFAULT_STATUS;;
     }
 
+    /**
+     * Prints the current status of the machine.
+     * Displays whether the machine is available or currently processing an item.
+     */
     public void printStatus() {
         if (available) {
             System.out.println("🟢 Machine " + this.id_machine + " is AVAILABLE");
@@ -64,45 +87,96 @@ public class Machine implements Comparable<Machine> {
         }
     }
 
-    // Set
+
+    //----------------Setters
+    /**
+     * Sets the current processing item for this machine.
+     *
+     * @param item the item currently being processed.
+     */
     public void setCurrentProcessingItem(Item item) {
         this.currentProcessingItem = item;
     }
 
+    /**
+     * Marks the machine as not available.
+     */
     public void setNotAvailable() {
         this.available = false;
     }
 
+    /**
+     * Sets the time left to finish processing based on the machine's processing speed.
+     */
     private void setTimeToFinish() {
         this.timeLeftToFinish = processingSpeed + 1;
     }
 
-    //-- Gets
+    //----------------Getters
+    /**
+     * Returns the operation associated with this machine.
+     *
+     * @return the operation of this machine.
+     */
     public Operation getOperation() {
         return operation;
     }
 
+    /**
+     * Returns the unique ID of this machine.
+     *
+     * @return the machine's ID.
+     */
     public ID getId_machine() {
         return id_machine;
     }
 
+    /**
+     * Returns the processing speed of this machine.
+     *
+     * @return the processing speed.
+     */
     public float getProcessingSpeed() {
         return processingSpeed;
     }
 
+    /**
+     * Checks if the machine is available.
+     *
+     * @return true if the machine is available; false otherwise.
+     */
     public boolean isAvailable() {
         return available;
     }
 
+    /**
+     * Returns the time left for this machine to finish processing.
+     *
+     * @return the time left to finish processing.
+     */
     public float getTimeLeftToFinish() {
         return timeLeftToFinish;
     }
 
+
+    /**
+     * Returns the item currently being processed by this machine.
+     *
+     * @return the current processing item.
+     */
     public Item getCurrentProcessingItem() {
         return currentProcessingItem;
     }
 
-    //-------- Methods of class Machine
+
+    //----------------Overrides and clone methods
+    /**
+     * Compares this machine to the specified object for equality.
+     * Two machines are considered equal if they have the same machine ID.
+     *
+     * @param o the object to compare this machine against.
+     * @return true if the specified object is equal to this machine; false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -112,11 +186,25 @@ public class Machine implements Comparable<Machine> {
         return Objects.equals(id_machine, machine.id_machine);
     }
 
+    /**
+     * Returns a hash code value for this machine.
+     * The hash code is based on the machine's unique ID.
+     *
+     * @return a hash code value for this machine.
+     */
     @Override
     public int hashCode() {
         return id_machine.hashCode();
     }
 
+    /**
+     * Compares this machine with another machine for order.
+     * Machines are compared based on their processing speed and then by their machine ID.
+     *
+     * @param o the machine to be compared.
+     * @return a negative integer, zero, or a positive integer as this machine
+     *         is less than, equal to, or greater than the specified machine.
+     */
     @Override
     public int compareTo(Machine o) {
         int speedComparison = Float.compare(this.processingSpeed, o.getProcessingSpeed());
@@ -126,6 +214,12 @@ public class Machine implements Comparable<Machine> {
         return this.id_machine.compareTo(o.getId_machine());
     }
 
+    /**
+     * Creates and returns a clone of this machine.
+     * The cloned machine has the same ID, operation, and processing speed.
+     *
+     * @return a clone of this machine.
+     */
     public Machine clone() {
         return new Machine(id_machine, operation, processingSpeed);
     }
