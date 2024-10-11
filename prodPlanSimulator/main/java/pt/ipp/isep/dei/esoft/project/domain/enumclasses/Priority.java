bc20324daf8e4190808347eefd5bb21246458c4b
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-10-11 16:25:21 +0100

    Priority/TypeID Classes - Documentation

--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/enumclasses/Priority.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/enumclasses/Priority.java
@@ -1,22 +1,47 @@
 package pt.ipp.isep.dei.esoft.project.domain.enumclasses;
 
+/**
+ * Represents the priority levels for operations.
+ * This enum defines three levels of priority: HIGH, MEDIUM, and LOW.
+ */
 public enum Priority {
     HIGH("High"),
     MEDIUM("Medium"),
     LOW("Low");
 
+    /**
+     * Constructs a Priority with the specified display name.
+     *
+     * @param displayName the display name of the priority.
+     */
     private final String displayName;
 
+    /**
+     * Returns the display name of this priority level.
+     *
+     * @return the display name of this priority level.
+     */
     @Override
     public String toString() {
         return displayName;
     }
 
+    /**
+     * Constructs a Priority with the specified display name.
+     *
+     * @param displayName the display name of the priority.
+     */
     Priority(String displayName) {
         this.displayName = displayName;
     }
 
 
+    /**
+     * Converts a string to its corresponding Priority enum.
+     *
+     * @param priority the string representation of the priority.
+     * @return the corresponding Priority enum, or null if no match is found or if the input is null.
+     */
     public static Priority fromString(String priority) {
         if (priority == null) {
             return null;
--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/enumclasses/TypeID.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/enumclasses/TypeID.java
@@ -1,5 +1,9 @@
 package pt.ipp.isep.dei.esoft.project.domain.enumclasses;
 
+/**
+ * Represents different types of IDs used in the system.
+ * This enum defines two types of IDs: MACHINE and ITEM.
+ */
 public enum TypeID {
     MACHINE{
         public String toString() {
