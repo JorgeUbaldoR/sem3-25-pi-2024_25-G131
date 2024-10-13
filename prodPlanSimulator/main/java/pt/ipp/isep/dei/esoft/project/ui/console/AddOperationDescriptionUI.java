4a8acc5721514c578659a8b499cab8bf49f395d5
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-10-13 15:37:39 +0100

    UI AddOperationDescriptionUI changes

--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/ui/console/AddOperationDescriptionUI.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/ui/console/AddOperationDescriptionUI.java
@@ -21,10 +21,9 @@ public class AddOperationDescriptionUI implements Runnable {
     }
 
     public void run() {
-        System.out.println("\n\n--- Add description to an operation ------------------------");
+        System.out.println("\n══════════════════════════════════════════");
+        System.out.println(ANSI_BRIGHT_WHITE+"      Change operation description       "+ANSI_RESET+"\n");
         addDescription();
-
-
     }
 
     private List<Operation> seeOperations() {
@@ -36,15 +35,14 @@ public class AddOperationDescriptionUI implements Runnable {
         }
         for (Operation operation : operationList) {
             if (operation.getOperationDescription() != null) {
-                System.out.printf("%s(%d)%s %s: %s%s%s%n", ANSI_PURPLE, c++, ANSI_RESET,
-                        operation.getOperationName(), ANSI_YELLOW,
+                System.out.printf("%s(%d)%s - %-10s %s %s\"%s\"%s%n", ANSI_BRIGHT_BLACK, c++, ANSI_RESET,
+                        operation.getOperationName(),":", ANSI_FOREST_GREEN,
                         operation.getOperationDescription(), ANSI_RESET);
 
             } else {
                 operation.setOperationDescription(ANSI_BRIGHT_RED + "No description provided!" + ANSI_RESET);
-                System.out.printf("%s(%d)%s %s: %s%s%s%n", ANSI_PURPLE, c++, ANSI_RESET,
-                        operation.getOperationName(), ANSI_YELLOW,
-                        operation.getOperationDescription(), ANSI_RESET);
+                System.out.printf("%s(%d)%s - %-10s %s %s%n", ANSI_BRIGHT_BLACK, c++, ANSI_RESET,
+                        operation.getOperationName(),":",operation.getOperationDescription());
             }
         }
         return operationList;
@@ -62,13 +60,13 @@ public class AddOperationDescriptionUI implements Runnable {
                 selection = scanner.nextInt();
 
                 if (selection <= 0 || selection > list.size()) {
-                    System.out.println(ANSI_ORANGE + "Select a valid number: " + ANSI_RESET);
+                    System.out.println(ANSI_LIGHT_RED + "\nSelect a valid number: " + ANSI_RESET);
                 } else {
                     valid = true;
                 }
 
             } catch (InputMismatchException e) {
-                System.out.println(ANSI_ORANGE + "Please select a NUMBER!" + ANSI_RESET);
+                System.out.println(ANSI_LIGHT_RED + "\nSelect a NUMBER!" + ANSI_RESET);
                 scanner.next();
             }
         } while (!valid);
@@ -83,27 +81,27 @@ public class AddOperationDescriptionUI implements Runnable {
             int option = selectOperation();
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Description to be added: ");
+            System.out.print("• Description to be added: ");
             String description = scanner.nextLine();
             String currentDescription = operationList.get(option - 1).getOperationDescription();
             String confirmation = requestConfirmation(description, operationList.get(option - 1).getOperationName());
 
             if (confirmation.equals("y")) {
                 if (operationList.get(option - 1).setOperationDescription(description))
-                    System.out.println(ANSI_BRIGHT_GREEN + "New description successfully added!" + ANSI_RESET);
+                    System.out.println(ANSI_BRIGHT_GREEN + "\nNew description successfully added!" + ANSI_RESET);
             } else {
                 operationList.get(option - 1).setOperationDescription(currentDescription);
-                System.out.println(ANSI_BRIGHT_RED + "No changes made!" + ANSI_RESET);
+                System.out.println(ANSI_LIGHT_RED + "\nNo changes made!" + ANSI_RESET);
             }
 
         } else
-            System.out.println(ANSI_BRIGHT_RED + "No operations in the system!" + ANSI_RESET);
+            System.out.println(ANSI_LIGHT_RED + "\nNo operations in the system!" + ANSI_RESET);
     }
 
     private String requestConfirmation(String answer, String operationName) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Are you sure that \"%s%s%s\" will be your %s%s%s operation description? (y/n): ",
-                ANSI_YELLOW, answer, ANSI_RESET, ANSI_YELLOW, operationName, ANSI_RESET);
+        System.out.printf("• Are you sure that \"%s%s%s\" will be your %s%s%s operation description? (y/n): ",
+                ANSI_FOREST_GREEN, answer, ANSI_RESET, ANSI_FOREST_GREEN, operationName, ANSI_RESET);
         String confirmation = scanner.nextLine();
         if (!confirmation.equalsIgnoreCase("y") && !confirmation.equalsIgnoreCase("n")) {
             do {
