aa2b50adfe74fab069e27a7d4038fbb4cefafc50
Francisco Santos <1230564@isep.ipp.pt>
2024-10-25 11:33:21 +0100

    us03 lapr

--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/FileDataReader.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/FileDataReader.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class FileDataReader {
     private static final int NUMBER_OF_DETAILS = 3;
-
+    private static final int BOM_DETAILS = 2;
 
     /**
      * Reads machine details from a CSV file and returns them as a list of string arrays
@@ -17,7 +17,7 @@ public class FileDataReader {
      * @throws IOException if an error occurs while reading the file
      */
     public static List<String[]> getMachinesDetails() throws IOException {
-        Scanner scanner = new Scanner(new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/workstations.csv"));
+        Scanner scanner = new Scanner(new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/workstations.csv"));
         List<String[]> machineDetails = new ArrayList<>();
 
         if (scanner.hasNextLine()) {
@@ -43,10 +43,7 @@ public class FileDataReader {
      * @throws IOException if the file is not found or another I/O error occurs
      */
     public static List<String[]> getItemsDetails() throws IOException {
-        File file = new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/articlesFinal.csv");
-        if (!file.exists()) {
-            throw new IOException("File not found: " + file.getAbsolutePath());
-        }
+        File file = new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/articlesFinal.csv");
 
         Scanner scanner = new Scanner(file);
         List<String[]> itemsDetails = new ArrayList<>();
@@ -67,4 +64,30 @@ public class FileDataReader {
         scanner.close();
         return itemsDetails;
     }
+
+
+
+    public static List<String[]> getBomDetails() throws IOException {
+        File file = new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/productStructure.csv");
+
+        Scanner scanner = new Scanner(file);
+        List<String[]> bom = new ArrayList<>();
+
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+
+            if (parts.length == (BOM_DETAILS)) {
+                bom.add(parts);
+            }
+        }
+
+        scanner.close();
+        return bom;
+    }
 }
+
