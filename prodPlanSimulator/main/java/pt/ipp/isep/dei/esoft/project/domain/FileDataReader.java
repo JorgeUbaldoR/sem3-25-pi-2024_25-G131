5e799f4d47591555ab90b9164ed79be1b8659cc5
Emanuel Almeida <1230839@isep.ipp.pt>
2024-10-25 14:37:41 +0100

    manual file input verification

--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/FileDataReader.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/FileDataReader.java
@@ -1,11 +1,14 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import static pt.ipp.isep.dei.esoft.project.domain.more.ColorfulOutput.*;
 
 public class FileDataReader {
     private static final int NUMBER_OF_DETAILS = 3;
@@ -16,6 +19,12 @@ public class FileDataReader {
     private static final int ID_ITEM = 1;
 
     private static final Boolean LEITURA_AUTOMATICA = false;
+
+    private static final String MACHINE_PATH = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/workstations.csv";
+    private static final String ITEM_PATH = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/articlesFinal.csv";
+    private static final String BOM_PATH = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/productStructure.csv";
+    private static final String RAW_PATH = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/";
+
 
     /**
      * Reads machine details from a CSV file and returns them as a list of string arrays
@@ -53,12 +62,11 @@ public class FileDataReader {
 
 
     /**
-     *
-     * @return  List of string arrays containing product and component
+     * @return List of string arrays containing product and component
      * @throws IOException if the file is not found or another I/O error occurs
      */
     public static List<String[]> getBomDetails() throws IOException {
-        File file = new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/productStructure.csv");
+        File file = new File(BOM_PATH);
 
         Scanner scanner = new Scanner(file);
         List<String[]> bom = new ArrayList<>();
@@ -80,8 +88,9 @@ public class FileDataReader {
         return bom;
     }
 
-    /** Reads details from a CSV file and returns them as a list of string arrays
-
+    /**
+     * Reads details from a CSV file and returns them as a list of string arrays
+     * <p>
      * Reads item details from a CSV file and returns them as a list of string arrays
      *
      * @return List of string arrays containing item details
@@ -117,32 +126,37 @@ public class FileDataReader {
     }
 
     private static boolean isValidFile(Boolean leituraAutomatica, int ID) {
+        String userNameFile = null;
 
-    /**
-     *
-     * @return  List of string arrays containing product and component
-     * @throws IOException if the file is not found or another I/O error occurs
-     */
+
         if (!leituraAutomatica) {
-            fileName = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/" + ler.nextLine();
+            userNameFile = ler.nextLine();
+            fileName = RAW_PATH + userNameFile;
         } else {
 
             if (ID == ID_MACHINE) {
-                fileName = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/workstations.csv";
+                fileName = MACHINE_PATH;
             } else {
-                fileName = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/articlesFinal.csv";
+                fileName = ITEM_PATH;
             }
 
         }
 
+        while (true) {
 
-        // Verifica se o arquivo existe
-        try {
-            new Scanner(new File(fileName)).close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Couldn't find file: " + fileName);
-            return false;
+            // Verifica se o arquivo existe
+
+            try {
+                new Scanner(new File(fileName)).close();
+                return true;
+
+            } catch (FileNotFoundException e) {
+                System.out.println(ANSI_BRIGHT_RED + "Couldn't find file: " + ANSI_RESET + userNameFile);
+                System.out.print("Try again: ");
+
+                fileName = RAW_PATH + ler.nextLine();
+
+            }
         }
-        return true;
     }
 }
