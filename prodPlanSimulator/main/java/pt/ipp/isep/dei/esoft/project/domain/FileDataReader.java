1b9f8e18fa974071c9fe14a526fdebe00e3c2550
Emanuel Almeida <1230839@isep.ipp.pt>
2024-10-25 11:49:00 +0100

    File Manual Input implemented

--- prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/FileDataReader.java
+++ prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/domain/FileDataReader.java
@@ -1,6 +1,7 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,12 @@ import java.util.Scanner;
 
 public class FileDataReader {
     private static final int NUMBER_OF_DETAILS = 3;
-    private static final int BOM_DETAILS = 2;
+    private static String fileName = null;
+    private static final Scanner ler = new Scanner(System.in);
+    private static final int ID_MACHINE = 0;
+    private static final int ID_ITEM = 1;
+
+    private static final Boolean LEITURA_AUTOMATICA = true;
 
     /**
      * Reads machine details from a CSV file and returns them as a list of string arrays
@@ -17,23 +23,31 @@ public class FileDataReader {
      * @throws IOException if an error occurs while reading the file
      */
     public static List<String[]> getMachinesDetails() throws IOException {
-        Scanner scanner = new Scanner(new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/workstations.csv"));
-        List<String[]> machineDetails = new ArrayList<>();
 
-        if (scanner.hasNextLine()) {
-            scanner.nextLine();
+        if (!LEITURA_AUTOMATICA) {
+            System.out.print("Write the name of the MACHINES file: ");
         }
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(";");
+        if (isValidFile(LEITURA_AUTOMATICA, ID_MACHINE)) {
+            Scanner scanner = new Scanner(new File(fileName));
+            List<String[]> machineDetails = new ArrayList<>();
 
-            if (parts.length == NUMBER_OF_DETAILS) {
-                machineDetails.add(parts);
+            if (scanner.hasNextLine()) {
+                scanner.nextLine();
             }
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(";");
+
+                if (parts.length == NUMBER_OF_DETAILS) {
+                    machineDetails.add(parts);
+                }
+            }
+            scanner.close();
+            return machineDetails;
         }
-        scanner.close();
-        return machineDetails;
+        return null;
     }
 
     /**
@@ -43,51 +57,57 @@ public class FileDataReader {
      * @throws IOException if the file is not found or another I/O error occurs
      */
     public static List<String[]> getItemsDetails() throws IOException {
-        File file = new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/articlesFinal.csv");
 
-        Scanner scanner = new Scanner(file);
-        List<String[]> itemsDetails = new ArrayList<>();
-
-        if (scanner.hasNextLine()) {
-            scanner.nextLine();
+        if (!LEITURA_AUTOMATICA) {
+            System.out.print("Write the name of the ITEMS file: ");
         }
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(";");
+        if (isValidFile(LEITURA_AUTOMATICA, ID_ITEM)) {
+            Scanner scanner = new Scanner(new File(fileName));
+            List<String[]> itemsDetails = new ArrayList<>();
 
-            if (parts.length >= NUMBER_OF_DETAILS) {
-                itemsDetails.add(parts);
+            if (scanner.hasNextLine()) {
+                scanner.nextLine();
             }
-        }
 
-        scanner.close();
-        return itemsDetails;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(";");
+
+                if (parts.length >= NUMBER_OF_DETAILS) {
+                    itemsDetails.add(parts);
+                }
+            }
+
+            scanner.close();
+            return itemsDetails;
+        }
+        return null;
     }
 
+    private static boolean isValidFile(Boolean leituraAutomatica, int ID) {
 
 
-    public static List<String[]> getBomDetails() throws IOException {
-        File file = new File("prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/input/productStructure.csv");
+        if (!leituraAutomatica) {
+            fileName = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/" + ler.nextLine();
+        } else {
 
-        Scanner scanner = new Scanner(file);
-        List<String[]> bom = new ArrayList<>();
-
-        if (scanner.hasNextLine()) {
-            scanner.nextLine();
-        }
-
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(",");
-
-            if (parts.length == (BOM_DETAILS)) {
-                bom.add(parts);
+            if (ID == ID_MACHINE) {
+                fileName = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/workstations.csv";
+            } else {
+                fileName = "prodPlanSimulator/main/java/pt/ipp/isep/dei/esoft/project/files/articlesFinal.csv";
             }
+
         }
 
-        scanner.close();
-        return bom;
+
+        // Verifica se o arquivo existe
+        try {
+            new Scanner(new File(fileName)).close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't find file: " + fileName);
+            return false;
+        }
+        return true;
     }
 }
-
