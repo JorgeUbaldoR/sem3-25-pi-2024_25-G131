77ab96103b87d9b1d92d9f2cc7b0536ceff2efe3
RomeuZouXu <francpok.zouxu4@gmail.com>
2024-11-16 15:34:39 +0000

    USAC05 Tests Implementation

--- plantFloorMachineManager(ARQCP)/USAC05/include/asm.h
+++ plantFloorMachineManager(ARQCP)/USAC05/include/asm.h
@@ -1,4 +1,4 @@
 #ifndef ASM_H
 #define ASM_H
-int enqueue_value(int* buffer, int length, int*tail, int* head, int value);
+	 int enqueue_value(int* buffer, int length, int*tail, int* head, int value);
 #endif
