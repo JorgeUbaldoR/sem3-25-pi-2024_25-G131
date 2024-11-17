2e2cc9bd4aa74e362d88968fa8209ebce61b1b1e
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-11-17 21:49:17 +0000

    USEI09 - Search By ID

--- prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/ui/console/SearchProductionTreeUI.java
+++ prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/ui/console/SearchProductionTreeUI.java
@@ -4,6 +4,7 @@ import pt.ipp.isep.dei.esoft.project.application.controller.ProductionTreeContro
 import pt.ipp.isep.dei.esoft.project.domain.ID;
 import pt.ipp.isep.dei.esoft.project.domain.Item;
 import pt.ipp.isep.dei.esoft.project.domain.TreeClasses.Node;
+import pt.ipp.isep.dei.esoft.project.domain.enumclasses.TypeID;
 
 import java.util.*;
 
@@ -27,15 +28,15 @@ public class SearchProductionTreeUI implements Runnable {
         System.out.println("\n\n══════════════════════════════════════════");
         System.out.println(ANSI_BRIGHT_WHITE + "        SEARCH OPERATION/MATERIAL                 " + ANSI_RESET + "\n");
         Scanner otherin = new Scanner(System.in);
-        System.out.printf("%s%s%s%n",ANSI_BRIGHT_BLACK,"prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/files/input/boo.csv",ANSI_RESET);
-        System.out.print("Enter the path to the boo.csv:");
-        String path = otherin.nextLine();
+        //System.out.printf("%s%s%s%n",ANSI_BRIGHT_BLACK,"prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/files/input/boo.csv",ANSI_RESET);
+        //System.out.print("Enter the path to the boo.csv:");
+        //String path = otherin.nextLine();
         System.out.printf("Select the processing order to be used:%n");
-        System.out.printf("     %s(1)%s - Search Material%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
-        System.out.printf("     %s(2)%s - Search Operation%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
-        System.out.printf("     %s(3)%s - Search by ID%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
-        System.out.printf("     %s(0)%s - Cancel%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
-        doChoice(getChoice(3), path);
+        System.out.printf(" %s(1)%s - Search Material%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
+        System.out.printf(" %s(2)%s - Search Operation%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
+        System.out.printf(" %s(3)%s - Search by ID%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
+        System.out.printf(" %s(0)%s - Cancel%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
+        doChoice(getChoice(3), "prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/files/input/boo.csv");
 
     }
 
@@ -43,7 +44,7 @@ public class SearchProductionTreeUI implements Runnable {
         int choice = 0;
         boolean valid = false;
         do {
-            System.out.printf("Type your choice: ");
+            System.out.printf(" %sType your choice:%s ", ANSI_BRIGHT_BLACK, ANSI_RESET);
             try {
                 choice = in.nextInt();
 
@@ -70,9 +71,20 @@ public class SearchProductionTreeUI implements Runnable {
                         break;
                     case 2:
                         ID selectedOperationID = showAndSelectOperation();
-                        Node node = getProductionTreeController().getNode(selectedOperationID);
-                        String parentName = getProductionTreeController().findParentOperation(node);
-                        printNodeOperationInf("OPERATION",node,parentName);
+                        searchInfOperation(selectedOperationID);
+                        break;
+                    case 3:
+                        System.out.printf("%n%s• SEARCH BY ID:%s%n", ANSI_BRIGHT_WHITE, ANSI_RESET);
+                        System.out.printf("%s-----------------------------------%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
+                        System.out.printf("%sExample of Input ->%s %sI-102%s %s|%s %sO-232%s%n%n", ANSI_BRIGHT_BLACK, ANSI_RESET, ANSI_BRIGHT_WHITE, ANSI_RESET, ANSI_BRIGHT_BLACK, ANSI_RESET, ANSI_BRIGHT_WHITE, ANSI_RESET);
+                        ID id = getInputID();
+                        if (id != null) {
+                            if(id.getTypeID() == TypeID.ITEM){
+
+                            }else{
+                                searchInfOperation(id);
+                            }
+                        }
                         break;
 
                     default:
@@ -88,17 +100,54 @@ public class SearchProductionTreeUI implements Runnable {
         }
     }
 
-    private void printNodeOperationInf(String type,Node node,String parentName) {
-        System.out.printf("%n%n%s========================================%s%n",ANSI_BRIGHT_BLACK,ANSI_RESET);
-        System.out.printf("   %sNODE INFORMATION | %s NODE%s%n",ANSI_BRIGHT_WHITE,type,ANSI_RESET);
-        System.out.printf("%s========================================%s%n",ANSI_BRIGHT_BLACK,ANSI_RESET);
-        System.out.printf("%s•%s Name Operation: %s%s%s%n",ANSI_BRIGHT_BLACK,ANSI_RESET,ANSI_BRIGHT_WHITE,getProductionTreeController().findNameOperation(node.getOperationID()),ANSI_RESET);
-        System.out.printf("%s•%s ID Operation: %s%s%s%n",ANSI_BRIGHT_BLACK,ANSI_RESET,ANSI_BRIGHT_WHITE,node.getOperationID(),ANSI_RESET);
-        System.out.printf("%s•%s Quantity: %sN/A%s%n",ANSI_BRIGHT_BLACK,ANSI_RESET,ANSI_BRIGHT_WHITE,ANSI_RESET);
-        if(parentName == null) {
-            System.out.printf("%s•%s Parent Operation: %sNone%s%n",ANSI_BRIGHT_BLACK,ANSI_RESET,ANSI_BRIGHT_RED,ANSI_RESET);
-        }else{
-            System.out.printf("%s•%s Parent Operation: %s%s%s%n",ANSI_BRIGHT_BLACK,ANSI_RESET,ANSI_BRIGHT_WHITE,parentName,ANSI_RESET);
+    private void searchInfOperation(ID selectedOperationID) {
+        Node node = getProductionTreeController().getNode(selectedOperationID);
+        String parentName = getProductionTreeController().findParentOperation(node);
+        printNodeOperationInf("OPERATION", node, parentName);
+    }
+
+    private ID getInputID() {
+        Scanner scanner = new Scanner(System.in);
+        String inputID;
+        System.out.print("Enter an ID: ");
+        inputID = scanner.nextLine();
+
+        if (!checkInputID(inputID)) {
+            do {
+                System.out.print("Enter an ID (follow the example): ");
+                inputID = scanner.nextLine();
+            } while (!checkInputID(inputID));
+        }
+        char type = inputID.charAt(0);
+        int serial = Integer.parseInt(inputID.split("-")[1]);
+        switch (Character.toUpperCase(type)) {
+            case 'I':
+                return new ID(serial, TypeID.ITEM);
+            case 'O':
+                return new ID(serial, TypeID.OPERATION);
+            default:
+                return null;
+        }
+    }
+
+    private boolean checkInputID(String inputID) {
+        char reference = inputID.charAt(0);
+        reference = Character.toUpperCase(reference);
+        return (reference == 'I' || reference == 'O') &&
+                inputID.charAt(1) == '-' && Character.isDigit(inputID.charAt(2));
+    }
+
+    private void printNodeOperationInf(String type, Node node, String parentName) {
+        System.out.printf("%n%n%s========================================%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
+        System.out.printf("   %sNODE INFORMATION | %s NODE%s%n", ANSI_BRIGHT_WHITE, type, ANSI_RESET);
+        System.out.printf("%s========================================%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
+        System.out.printf("%s•%s Name Operation: %s%s%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET, ANSI_BRIGHT_WHITE, getProductionTreeController().findNameOperation(node.getOperationID()), ANSI_RESET);
+        System.out.printf("%s•%s ID Operation: %s%s%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET, ANSI_BRIGHT_WHITE, node.getOperationID(), ANSI_RESET);
+        System.out.printf("%s•%s Quantity: %sN/A%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET, ANSI_BRIGHT_WHITE, ANSI_RESET);
+        if (parentName == null) {
+            System.out.printf("%s•%s Parent Operation: %sNone%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET, ANSI_BRIGHT_RED, ANSI_RESET);
+        } else {
+            System.out.printf("%s•%s Parent Operation: %s%s%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET, ANSI_BRIGHT_WHITE, parentName, ANSI_RESET);
         }
 
 
@@ -109,22 +158,21 @@ public class SearchProductionTreeUI implements Runnable {
         System.out.printf("%s-----------------------------------%s%n", ANSI_BRIGHT_BLACK, ANSI_RESET);
         Map<ID, String> listOperations = getProductionTreeController().getListToShow(0);
 
-        Map<Integer, Map.Entry<ID,String>> options = showOptions(listOperations);
+        Map<Integer, Map.Entry<ID, String>> options = showOptions(listOperations);
         int choice = getChoice(listOperations.size() - 1);
 
         return options.get(choice).getKey();
     }
 
 
-
-    private  Map<Integer, Map.Entry<ID,String>> showOptions(Map<ID, String> list) {
+    private Map<Integer, Map.Entry<ID, String>> showOptions(Map<ID, String> list) {
         int index = 0;
-        Map<Integer, Map.Entry<ID,String>> options = new HashMap<>();
-        for (Map.Entry<ID,String> entry : list.entrySet()) {
+        Map<Integer, Map.Entry<ID, String>> options = new HashMap<>();
+        for (Map.Entry<ID, String> entry : list.entrySet()) {
             System.out.printf("%s(%-3s%s - %sID:%s %s -> %sName:%s %s%n",
                     ANSI_BRIGHT_BLACK, index + ")", ANSI_RESET,
-                    ANSI_BRIGHT_BLACK,ANSI_RESET,entry.getKey(),
-                    ANSI_BRIGHT_BLACK,ANSI_RESET,entry.getValue());
+                    ANSI_BRIGHT_BLACK, ANSI_RESET, entry.getKey(),
+                    ANSI_BRIGHT_BLACK, ANSI_RESET, entry.getValue());
             options.put(index, entry);
             index++;
         }
