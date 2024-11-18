c03ad37930a14f8f670afe9caa45f185f8eb7ede
Emanuel Almeida <1230839@isep.ipp.pt>
2024-11-18 20:19:04 +0000

    US08 - asm.h

--- plantFloorMachineManager(ARQCP)/USAC06/include/asm.h
+++ plantFloorMachineManager(ARQCP)/USAC06/include/asm.h
@@ -1,5 +1,7 @@
 #ifndef ASM_H
 #define ASM_H
-int dequeue_value (int* a, int b, int* c, int* d, int* e);
+
+int move_n_to_array(int* buffer, int length, int *tail, int *head, int n, int* array);
 int get_n_element (int* a, int b, int* c, int* d);
+
 #endif
