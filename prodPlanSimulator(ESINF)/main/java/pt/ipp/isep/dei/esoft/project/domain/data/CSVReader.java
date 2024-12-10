885763cd98635f8ee14e5999dbf0b541687393a6
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-12-10 22:39:33 +0000

    USEI17 - Validations

--- prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/domain/data/CSVReader.java
+++ prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/domain/data/CSVReader.java
@@ -17,44 +17,110 @@ import java.util.Map;
 public class CSVReader {
     private static final int START_OF_PREDECESSORS = 6;
 
-    public static MapGraph<Activity,Double> readCSV(String filePath) throws Exception {
-        MapGraph<Activity,Double> graph = new MapGraph<>(true);
-        Reader reader = new FileReader(filePath);
+    public static MapGraph<Activity, Double> readCSV(String filePath) {
+        MapGraph<Activity, Double> graph = new MapGraph<>(true);
 
-        Iterable<CSVRecord> records = CSVFormat.DEFAULT
-            .withHeader("act_id", "act_descr", "duration", "duration_unit", "cost", "cost_unit", "prev_act_id1", "prev_act_id2")
-            .withSkipHeaderRecord()
-            .parse(reader);
+        try {
+            Reader reader = new FileReader(filePath);
 
-        Map<ID,Activity> activityMap = new HashMap<>();
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT
+                    .withHeader("act_id", "act_descr", "duration", "duration_unit", "cost", "cost_unit", "prev_act_id1", "prev_act_id2")
+                    .withSkipHeaderRecord()
+                    .parse(reader);
 
-        for (CSVRecord record : records) {
-            String id = record.get("act_id");
-            String description = record.get("act_descr");
-            double duration = Double.parseDouble(record.get("duration"));
-            String durationUnit = record.get("duration_unit");
-            double cost = Double.parseDouble(record.get("cost"));
-            String costUnit = record.get("cost_unit");
-            List<ID> predecessors = new ArrayList<>();
+            Map<ID, Activity> activityMap = new HashMap<>();
 
-            for (int i = START_OF_PREDECESSORS; i < record.size(); i++) { // Assuming dependencies start from column 6
-                if (!record.get(i).isEmpty()) {
-                    predecessors.add( new ID(Integer.parseInt(record.get(i)), TypeID.ACTIVITY) );
+            for (CSVRecord record : records) {
+                String id = record.get("act_id");
+                String description = record.get("act_descr");
+                String duration = (record.get("duration")); //
+                String durationUnit = record.get("duration_unit");
+                String cost = (record.get("cost")); //
+                String costUnit = record.get("cost_unit");
+                List<ID> predecessors = new ArrayList<>();
+
+                validateParametersUnits(id, duration, cost);
+
+
+                for (int i = START_OF_PREDECESSORS; i < record.size(); i++) { // Assuming dependencies start from column 6
+                    if (!record.get(i).isEmpty()) {
+                        String idPredecessor = record.get(i);
+                        checkString(idPredecessor,"PREDECESSOR");
+                        checkConversionAndNegativeDouble(idPredecessor,"PREDECESSOR");
+                        predecessors.add(new ID(Integer.parseInt(idPredecessor), TypeID.ACTIVITY));
+                    }
+                }
+
+                Activity activity = new Activity(new ID(Integer.parseInt(id.trim()), TypeID.ACTIVITY), description, Double.parseDouble(duration.trim()), durationUnit, Double.parseDouble(cost.trim()), costUnit, predecessors);
+                activityMap.put(activity.getId(), activity);
+                graph.addVertex(activity);
+
+                for (ID predID : predecessors) {
+                    Activity predecessor = activityMap.get(predID);
+                    if (predecessor != null) {
+                        graph.addEdge(predecessor, activity, predecessor.getDuration());
+                    }
                 }
             }
+            return graph;
 
-            Activity activity = new Activity(new ID(Integer.parseInt(id), TypeID.ACTIVITY), description, duration, durationUnit, cost, costUnit, predecessors);
-            activityMap.put(activity.getId(),activity);
-            graph.addVertex(activity);
-
-            for(ID predID :predecessors){
-                Activity predecessor = activityMap.get(predID);
-                if(predecessor != null){
-                    graph.addEdge(predecessor,activity,predecessor.getDuration());
-                }
-            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error processing CSV file: " + e.getMessage());
         }
-        return graph;
     }
 
+    private static void validateParametersUnits(String id, String duration, String cost) {
+        checkString(id,"ID");
+        checkString(duration,"DURATION");
+        checkString(cost,"COST");
+        checkConverssion(id, duration, cost);
+    }
+
+    private static void checkString(String param, String token) {
+        if (!validateString(param)) {
+            throw new IllegalArgumentException("Invalid "+token+" - make sure is not Empty or Null");
+        }
+    }
+
+    private static void checkConverssion(String id, String duration, String cost) {
+        checkConversionAndNegativeInt(id,"ID");
+        checkConversionAndNegativeDouble(duration,"DURATION");
+        checkConversionAndNegativeDouble(cost,"COST");
+    }
+
+    private static void checkConversionAndNegativeInt(String param,String token){
+        try {
+            if(Integer.parseInt(param) < 0){
+                sendNegativeError(token,param);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid "+token+" -> [" + param + "]  is not a number.");
+        }
+    }
+
+    private static void checkConversionAndNegativeDouble(String param,String token){
+        try {
+            if(Double.parseDouble(param) < 0){
+                sendNegativeError(token,param);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid "+token+" -> [" + param + "]  is not a number.");
+        }
+    }
+
+    private static void sendNegativeError(String token, String param) {
+        throw new IllegalArgumentException("Invalid "+token+" -> [" + param + "]  cannot be negative.");
+    }
+
+    //-------------------------- String Validations ---------------------------------------------
+
+    /**
+     * Validates if a string is not null or empty.
+     *
+     * @param string The string to be validated.
+     * @return true if the string is not null or empty, false otherwise.
+     */
+    public static boolean validateString(String string) {
+        return (string != null && !string.trim().isEmpty());
+    }
 }
